ddb186208c77c166e973e2d0c426a0e5cfacf639
cmatula <90786977+cmatula@users.noreply.github.com>
2021-09-24 11:17:50 +0200

    Cleanup HTML, JS, CSS

--- src/javascript.js
+++ src/javascript.js
@@ -30,7 +30,6 @@ function formatDay(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
